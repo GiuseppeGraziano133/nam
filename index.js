9230db7f8fdb21a8f0ad32584529679acf73d98b
Giuseppe Graziano <giuseppe.graziano@eng.it>
2024-02-26 21:45:40 +0100

    better args names

--- index.js
+++ index.js
@@ -14,27 +14,27 @@ const versionsFilePath = path.join(__dirname, 'versions.json');
 yargs
     .scriptName("nam")
     .usage('$0 <cmd> [args]')
-    .command('add <vers> <name>', 'Add a new alias for a Node version', (yargs) => {
+    .command('add <vers> <alias>', 'Add a new alias for a Node version', (yargs) => {
         yargs.positional('vers', {
             describe: 'Version of Node to use',
             type: 'string'
-        }).positional('name', {
+        }).positional('alias', {
             describe: 'Alias to use',
             type: 'string'
         })
     }, (argv) => {
         add(argv.vers, argv.name, versionsFilePath);
     })
-    .command('remove <name>', 'Remove an alias for a Node version', (yargs) => {
-        yargs.positional('name', {
+    .command('remove <alias>', 'Remove an alias for a Node version', (yargs) => {
+        yargs.positional('alias', {
             describe: 'Alias to use',
             type: 'string'
         })
     }, (argv) => {
         remove(argv.name, versionsFilePath);
     })
-    .command('use <name>', 'Select a version using its alias and use it', (yargs) => {
-        yargs.positional('name', {
+    .command('use <alias>', 'Select a version using its alias and use it', (yargs) => {
+        yargs.positional('alias', {
             describe: 'Alias to use',
             type: 'string'
         })
